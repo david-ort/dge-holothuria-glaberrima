3cac88a2e7c8d225cc42bfe7feb09e4f67ddcade
David John <david.ortiz11@upr.edu>
2017-07-01 19:02:37 -0400

    Create volcanoplotday20.R

---
+++ DGE in Holothuria glaberrima/volcanoplotday20.R
@@ -0,0 +1,38 @@
+library(DESeq2)
+library(Biobase)
+library(biomaRt)
+library(limma)
+library(edgeR)
+
+day12 <- read.table("day20VSunin.txt", header = TRUE, row.names = 1)
+groups <- read.table("data20.txt", header = TRUE, row.names = 1)
+
+head(day20)
+dim(day20)
+head(groups)
+
+
+isexpr <- rowSums(cpm(day20)>1)>=3
+x <- day20[isexpr,]
+sum(isexpr)
+dim(x)
+
+head(isexpr)
+
+nf <- calcNormFactors(x)
+gr <- groups$Group
+design <- model.matrix(~ gr)
+head(gr)
+dim(design)
+
+y <- voom(x,design,plot=TRUE, lib.size=colSums(x)*nf,normalize.method="quantile")
+
+dim(y)
+
+fit <- lmFit(y,design)
+fit <- eBayes(fit)
+topTable(fit, coef=2)
+
+pdf("volcano20.pdf")
+volcanoplot(fit,coef=2)
+dev.off()
