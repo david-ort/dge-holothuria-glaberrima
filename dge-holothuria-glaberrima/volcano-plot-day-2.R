e8ed4852e601cbe336e5872fa6fc4bcbc5841630
David John Ortiz Rivera <david.ortiz11@upr.edu>
2018-04-03 08:31:08 -0400

    Create volcano-plot-day-2.R

---
+++ dge-holothuria-glaberrima/volcano-plot-day-2.R
@@ -0,0 +1,30 @@
+#
+# Author      : David J. Ortiz Rivera
+# Project     : Differential Gene Expression in Holothuria Glaberrima
+# File        : volcano-plot-day-2.R
+# Description : Create Volcano plot for Day 2 and Uninjured Normalized.
+
+library(DESeq2)
+library(Biobase)
+library(biomaRt)
+library(limma)
+library(edgeR)
+
+day2 <- read.table("day2VSunin.txt", header = TRUE, row.names = 1)
+
+groups <- read.table("datad2.txt", header = TRUE, row.names = 1)
+isexpr <- rowSums(cpm(day2)>1)>=3
+x <- day2[isexpr,]
+nf <- calcNormFactors(x)
+gr <- groups$Group
+design <- model.matrix(~ gr)
+
+y <- voom(x,design,plot=TRUE, lib.size=colSums(x)*nf,normalize.method="quantile")
+
+fit <- lmFit(y,design)
+fit <- eBayes(fit)
+topTable(fit, coef=2)
+
+pdf("volcano2.pdf")
+volcanoplot(fit,coef=2)
+dev.off()
