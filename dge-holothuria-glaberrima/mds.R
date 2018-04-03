44ce24969cd637d7e18ffbfb4e3a9c069cd120a9
David John Ortiz Rivera <david.ortiz11@upr.edu>
2018-04-03 08:11:17 -0400

    Update mds.R

--- dge-holothuria-glaberrima/mds.R
+++ dge-holothuria-glaberrima/mds.R
@@ -1,7 +1,8 @@
-/*
-Comment
-*/
-
+#
+# Author      : David J. Ortiz Rivera
+# Project     : Differential Gene Expression in Holothuria Glaberrima
+# File        : mds.R
+# Description : Create MDS plot for all groups (Day 2, Day 12, Day 20, and Uninjured)
 
 library(methods)
 library(Biobase)
@@ -9,29 +10,21 @@ library(biomaRt)
 library("edgeR")
 
 files <- c("SRR490924_quant.counts",
-		   "SRR490923_quant.counts",
-		   "SRR490921_quant.counts",
-		   "SRR490920_quant.counts",
-		   "SRR490919_quant.counts",
-		   "SRR490918_quant.counts",
-		   "SRR490868_quant.counts",
-		   "SRR490864_quant.counts")
+           "SRR490923_quant.counts",
+	   "SRR490921_quant.counts",
+	   "SRR490920_quant.counts",
+	   "SRR490919_quant.counts",
+	   "SRR490918_quant.counts",
+	   "SRR490868_quant.counts",
+	   "SRR490864_quant.counts")
 
 labels=c("SRR490924","SRR490923",
-		 "SRR490921","SRR490920",
-		 "SRR490919", "SRR490918", 
+	 "SRR490921","SRR490920",
+	 "SRR490919", "SRR490918", 
          "SRR490868", "SRR490864")
 
 data <- readDGE(files)
-
-print(data)
-head(data$counts)
-
 group <- c(rep("Day_20_Post-injury", 2), rep("Day_12_Post-injury", 2), rep("Day_2_Post-injury", 2), rep("Uninjured_Radial_Organ",2))
-
-dim(data)
-length(group)
-
 dge = DGEList(counts=data, group=group)
 dge <- estimateCommonDisp(dge)
 dge <- estimateTagwiseDisp(dge)
